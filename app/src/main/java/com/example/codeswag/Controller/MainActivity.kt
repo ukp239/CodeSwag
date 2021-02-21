912638725eb6203df683d9ed311d14fdb8cc5e21
ukp239 <ukp239@gmail.com>
2021-02-21 14:05:10 +0530

    Lesson4_v2

--- app/src/main/java/com/example/codeswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/codeswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.codeswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.codeswag.Adapters.CategoryAdapter
 import com.example.codeswag.Model.Category
 import com.example.codeswag.R
@@ -23,5 +24,10 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this, DataService.category)
 
         categoryListView.adapter = adapter //setting the adapter for the list view from activity_main to the adapter created
+
+        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+            val category = DataService.category[i]
+            Toast.makeText(this, "You clicked on ${category} item", Toast.LENGTH_SHORT).show()
+        }
     }
 }
